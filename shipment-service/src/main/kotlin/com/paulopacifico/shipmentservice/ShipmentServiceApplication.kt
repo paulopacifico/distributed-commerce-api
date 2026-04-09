34857bd9d8de4f5b9531da13f1b139c231c6acd9
paulopacifico <paulo_pacifico@outlook.com>
2026-04-09 09:56:36 -0400

    Fix integration test container configuration

--- shipment-service/src/main/kotlin/com/paulopacifico/shipmentservice/ShipmentServiceApplication.kt
+++ shipment-service/src/main/kotlin/com/paulopacifico/shipmentservice/ShipmentServiceApplication.kt
@@ -1,9 +1,11 @@
 package com.paulopacifico.shipmentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ShipmentServiceApplication
 
 fun main(args: Array<String>) {
